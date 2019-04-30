05093dc1651c77b59237a078b7dce2001b966b03
Thiago Galvani <thiagopaiva99@gmail.com>
2019-04-29 21:32:10 -0300

    Hanlding request errors

--- api/app/controllers/api/v1/api_controller.rb
+++ api/app/controllers/api/v1/api_controller.rb
@@ -2,7 +2,17 @@ module Api::V1
 
  class ApiController < ApplicationController
  
-   # Global Methods
+   rescue_from(ActiveRecord::RecordNotFound) do ||
+      render(json: {
+        message: 'Not Found'
+      }, status: :not_found)
+   end
+ 
+   rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+      render(json: {
+        message: "Required parameter missing: #{parameter_missing_exception.param}"
+      }, status: :bad_request)
+   end
  
  end
 
